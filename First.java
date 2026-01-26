cab3088cf5ebed3a08b2e82dbbea34dd9e5192c9
shipman-delmas <delmas3ship@gmail.com>
2026-01-26 15:12:21 -0500

    Module 1 Assignment 1

---
+++ First.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Delmas Shipman
+
+        System.out.println("Hola mundo!");
+        System.out.println("Job Title: Software Engineer");
+        System.out.println("Why: I enjoy coding.");
+        System.out.println("Median Salary: $131,450");
+
+    }
+}
