39ee7b048e72f50f8327cc1faddcf6bfbed1ada5
shipman-delmas <delmas3ship@gmail.com>
2026-02-23 13:50:51 -0500

    Module 3 Assignment 2
    Module 3 Assignment 2

---
+++ M3A1_Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+    public static void main(String[] args) {
+        Tests myTests = new Tests();
+        myTests.getAverage();
+
+        System.out.println(myTests.toString());
+    }
+}
